644d1c6aa4c2c52cd80749fceaec212235db60db
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-01-16 16:08:17 +0000

    add profile icon

--- cv-builder/src/components/Header.jsx
+++ cv-builder/src/components/Header.jsx
@@ -1,11 +1,17 @@
 import HeaderCSS from "../styles/Header.module.css";
+import { CgProfile } from "react-icons/cg";
+import { IconContext } from "react-icons";
 
 export default function Header() {
   return (
     <>
       <header className={HeaderCSS.header}>
         <h1>CanVas</h1>
-        <button className={HeaderCSS.btn}>Create CV</button>
+        <IconContext.Provider
+          value={{ size: "2rem", className: HeaderCSS.profileIcon }}
+        >
+          <CgProfile />
+        </IconContext.Provider>
       </header>
     </>
   );
