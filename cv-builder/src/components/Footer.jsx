e58638303195d6851f21fe25ec33510f08deb716
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-01-16 13:08:34 +0000

    change global selectors to class names

--- cv-builder/src/components/Footer.jsx
+++ cv-builder/src/components/Footer.jsx
@@ -9,20 +9,20 @@ export default function Footer() {
       <div className={footerCSS.footerWrapper}>
         <nav className={footerCSS.footerNav}>
           <ul>
-            <li>
+            <li className={footerCSS.footerLi}>
               <a href="">Twitter</a>
             </li>
-            <li>
+            <li className={footerCSS.footerLi}>
               <a href="">Instagram</a>
             </li>
-            <li>
+            <li className={footerCSS.footerLi}>
               <a href="">Facebook</a>
             </li>
           </ul>
         </nav>
         <div className={footerCSS.footerBottom}>
-          <p>Copyright &copy; {currYear}</p>
-          <p>Made by Alan Dylewski</p>
+          <p className={footerCSS.footerP}>Copyright &copy; {currYear}</p>
+          <p className={footerCSS.footerP}>Made by Alan Dylewski</p>
         </div>
       </div>
     </footer>
