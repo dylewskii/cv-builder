b04f9ba71d12d9df9c56a84e4cbc0eada4aff2e3
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-01-16 10:51:28 +0000

    add CV component

--- cv-builder/src/App.jsx
+++ cv-builder/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./components/Header";
 import FilterableCVTable from "./components/FilterableCVTable";
+import CV from "./components/CV";
 
 export default function App() {
   return (
     <>
       <Header />
-      <FilterableCVTable></FilterableCVTable>
+      <FilterableCVTable />
+      <CV />
     </>
   );
 }
---
+++ cv-builder/src/components/CV.jsx
@@ -0,0 +1,57 @@
+import cvCSS from "../styles/CV.module.css";
+
+export default function CV() {
+  return (
+    <main>
+      <section className={cvCSS.wrapper}>
+        <div className={cvCSS.cvPreview}>
+          <img alt="CV preview"></img>
+        </div>
+        <div className={cvCSS.cvOptions}>
+          <p className={cvCSS.name}>Jon Jones</p>
+          <p>Created: </p>
+          <nav>
+            <ul>
+              <li>
+                <a>Preview</a>
+              </li>
+              <li>
+                <a>Edit</a>
+              </li>
+              <li>
+                <a>Download PDF</a>
+              </li>
+              <li>
+                <a>Delete</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </section>
+
+      <section className={cvCSS.wrapper}>
+        <div className={cvCSS.cvPreview}>
+          <svg
+            className={cvCSS.plusIcon}
+            xmlns="http://www.w3.org/2000/svg"
+            height="24"
+            viewBox="0 -960 960 960"
+            width="24"
+          >
+            <path
+              fill="white"
+              d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z"
+            />
+          </svg>
+        </div>
+        <div className={cvCSS.cvOptions}>
+          <p className={cvCSS.name}>Create a CV</p>
+          <p>
+            Create a tailored CV for each job application - increase your
+            chances of getting hired!
+          </p>
+        </div>
+      </section>
+    </main>
+  );
+}
