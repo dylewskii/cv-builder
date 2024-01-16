bd7c62b15d33e778f31ff4912a43a8c2bd9df3da
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-01-16 13:01:01 +0000

    add footer content & styles

--- cv-builder/src/components/Footer.jsx
+++ cv-builder/src/components/Footer.jsx
@@ -1,10 +1,30 @@
 import footerCSS from "../styles/Footer.module.css";
 
 export default function Footer() {
+  const date = new Date();
+  const currYear = date.getFullYear();
+
   return (
-    <footer className={footerCSS.footer}>
-      <p>Follow us:</p>
-      <a>Github</a>
+    <footer>
+      <div className={footerCSS.footerWrapper}>
+        <nav className={footerCSS.footerNav}>
+          <ul>
+            <li>
+              <a href="">Twitter</a>
+            </li>
+            <li>
+              <a href="">Instagram</a>
+            </li>
+            <li>
+              <a href="">Facebook</a>
+            </li>
+          </ul>
+        </nav>
+        <div className={footerCSS.footerBottom}>
+          <p>Copyright &copy; {currYear}</p>
+          <p>Made by Alan Dylewski</p>
+        </div>
+      </div>
     </footer>
   );
 }
