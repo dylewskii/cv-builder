3192704d180a75b2c5d248df5fee9812621aebe4
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-01-17 12:57:54 +0000

    add animations & transitions to btns, links and icons

--- cv-builder/src/components/CV.jsx
+++ cv-builder/src/components/CV.jsx
@@ -39,6 +39,7 @@ export default function CV() {
             width="48"
           >
             <path
+              className={cvCSS.plusIconPath}
               fill="white"
               d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z"
             />
