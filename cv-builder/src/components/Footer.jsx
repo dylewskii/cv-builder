3d59976028af72cc54d08c11ebb467218d86cf1a
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-01-17 13:08:20 +0000

    add social icons to footer & position

--- cv-builder/src/components/Footer.jsx
+++ cv-builder/src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import footerCSS from "../styles/Footer.module.css";
+import { FaTwitter, FaInstagram, FaFacebook } from "react-icons/fa";
 
 export default function Footer() {
   const date = new Date();
@@ -10,12 +11,15 @@ export default function Footer() {
         <nav className={footerCSS.footerNav}>
           <ul>
             <li className={footerCSS.footerLi}>
+              <FaTwitter />
               <a href="">Twitter</a>
             </li>
             <li className={footerCSS.footerLi}>
+              <FaInstagram />
               <a href="">Instagram</a>
             </li>
             <li className={footerCSS.footerLi}>
+              <FaFacebook />
               <a href="">Facebook</a>
             </li>
           </ul>
