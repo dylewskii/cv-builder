c157cdb0da7c0051389a7862172bdb9839fee90f
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-01-17 16:08:30 +0000

    position footer

--- cv-builder/src/components/Footer.jsx
+++ cv-builder/src/components/Footer.jsx
@@ -6,28 +6,26 @@ export default function Footer() {
   const currYear = date.getFullYear();
 
   return (
-    <footer>
-      <div className={footerCSS.footerWrapper}>
-        <nav className={footerCSS.footerNav}>
-          <ul>
-            <li className={footerCSS.footerLi}>
-              <FaTwitter />
-              <a href="">Twitter</a>
-            </li>
-            <li className={footerCSS.footerLi}>
-              <FaInstagram />
-              <a href="">Instagram</a>
-            </li>
-            <li className={footerCSS.footerLi}>
-              <FaFacebook />
-              <a href="">Facebook</a>
-            </li>
-          </ul>
-        </nav>
-        <div className={footerCSS.footerBottom}>
-          <p className={footerCSS.footerP}>Copyright &copy; {currYear}</p>
-          <p className={footerCSS.footerP}>Made by Alan Dylewski</p>
-        </div>
+    <footer className={footerCSS.footer}>
+      <nav className={footerCSS.footerNav}>
+        <ul>
+          <li className={footerCSS.footerLi}>
+            <FaTwitter />
+            <a href="">Twitter</a>
+          </li>
+          <li className={footerCSS.footerLi}>
+            <FaInstagram />
+            <a href="">Instagram</a>
+          </li>
+          <li className={footerCSS.footerLi}>
+            <FaFacebook />
+            <a href="">Facebook</a>
+          </li>
+        </ul>
+      </nav>
+      <div className={footerCSS.footerBottom}>
+        <p className={footerCSS.footerP}>Copyright &copy; {currYear}</p>
+        <p className={footerCSS.footerP}>Made by Alan Dylewski</p>
       </div>
     </footer>
   );
