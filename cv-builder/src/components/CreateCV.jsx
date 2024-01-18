dcc9e1f1bd2aad6c5e7a7681f64cd3612ad03cbc
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-01-18 10:00:27 +0000

    add controlled components

--- cv-builder/src/components/CreateCV.jsx
+++ cv-builder/src/components/CreateCV.jsx
@@ -27,18 +27,24 @@ function PersonalDetails({ fName, lName, email, tel, country, city }) {
   return (
     <>
       <h3>Personal Details</h3>
+
       <label>First Name</label>
-      <input type="text">{fName}</input>
+      <input type="text" value={fName} />
+
       <label>Last Name</label>
-      <input type="text">{lName}</input>
+      <input type="text" value={lName} />
+
       <label>Email</label>
-      <input type="email">{email}</input>
+      <input type="email" value={email} />
+
       <label>Phone</label>
-      <input type="tel">{tel}</input>
+      <input type="tel" value={tel} />
+
       <label>Country</label>
-      <input type="text">{country}</input>
+      <input type="text" value={country} />
+
       <label>City</label>
-      <input type="text">{city}</input>
+      <input type="text" value={city} />
     </>
   );
 }
@@ -52,9 +58,7 @@ function ProfessionalSummary({ summary }) {
         & most importantly - your biggest achievements, best qualities and
         skills.
       </p>
-      <textarea name="" id="" cols="30" rows="4">
-        {summary}
-      </textarea>
+      <textarea value={summary} name="" id="" cols="30" rows="4" />
     </>
   );
 }
@@ -70,18 +74,21 @@ function EmploymentHistory({
     <>
       <h3>Employment History</h3>
       <p>Show your relevant experience.</p>
+
       <label>Job Title</label>
-      <input type="text">{jobTitle}</input>
+      <input type="text" value={jobTitle} />
+
       <label>Employer</label>
-      <input type="text">{employer}</input>
+      <input type="text" value={employer} />
+
       <label>Start & End Date</label>
-      <input type="text" placeholder="MM/YY - MM/YY">
-        {dateRange}
-      </input>
+      <input type="text" placeholder="MM/YY - MM/YY" value={dateRange} />
+
       <label>City</label>
-      <input type="text">{city}</input>
+      <input type="text" value={city} />
+
       <label>Description</label>
-      <input type="text">{description}</input>
+      <input type="text" value={description} />
     </>
   );
 }
@@ -94,18 +101,21 @@ function Education({ school, degree, dateRange, city, description }) {
         Sum up the value that your learnings and background will bring to the
         job.
       </p>
+
       <label>School</label>
-      <input type="text">{school}</input>
+      <input type="text" value={school} />
+
       <label>Degree</label>
-      <input type="text">{degree}</input>
+      <input type="text" value={degree} />
+
       <label>Start & End Date</label>
-      <input type="text" placeholder="MM/YY - MM/YY">
-        {dateRange}
-      </input>
+      <input type="text" placeholder="MM/YY - MM/YY" value={dateRange} />
+
       <label>City</label>
-      <input type="text">{city}</input>
+      <input type="text" value={city} />
+
       <label>Description</label>
-      <input type="text">{description}</input>
+      <input type="text" value={description} />
     </>
   );
 }
@@ -115,27 +125,35 @@ function Skills({ skill, level }) {
     <>
       <h3>Skills</h3>
       <p>List your most desirable skills.</p>
+
       <label>Skill</label>
-      <input type="text">{skill}</input>
+      <input type="text" value={skill} />
+
       <label>Level</label>
-      <input type="range">{level}</input>
+      <input type="range" value={level} />
     </>
   );
 }
 
-function References({ referent, company, phone, email }) {
+function References({ hide, referent, company, phone, email }) {
   return (
     <>
       <h3>References</h3>
-      <input type="radio"></input>
+
+      <label>Available upon request only.</label>
+      <input type="radio" value={hide} />
+
       <label>Referent&#39;s Full Name</label>
-      <input type="text">{referent}</input>
+      <input type="text" value={referent} />
+
       <label>Company</label>
-      <input type="text">{company}</input>
+      <input type="text" value={company} />
+
       <label>Phone</label>
-      <input type="tel">{phone}</input>
+      <input type="tel" value={phone} />
+
       <label>Email</label>
-      <input type="email">{email}</input>
+      <input type="email" value={email} />
     </>
   );
 }
