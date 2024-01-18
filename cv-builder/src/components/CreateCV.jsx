2fda593b0255fda46d3cb65530fd3b8f7a768836
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-01-18 10:33:14 +0000

    add controlled components

--- cv-builder/src/components/CreateCV.jsx
+++ cv-builder/src/components/CreateCV.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import createCSS from "../styles/CreateCV.module.css";
 
 export default function CreateCV() {
@@ -14,8 +15,6 @@ export default function CreateCV() {
 
           <Education />
 
-          <Skills />
-
           <References />
         </form>
       </section>
@@ -23,33 +22,66 @@ export default function CreateCV() {
   );
 }
 
-function PersonalDetails({ fName, lName, email, tel, country, city }) {
+function PersonalDetails() {
+  const [details, setDetails] = useState({
+    fName: "",
+    lName: "",
+    email: "",
+    tel: "",
+    city: "",
+    country: "",
+  });
   return (
     <>
       <h3>Personal Details</h3>
 
       <label>First Name</label>
-      <input type="text" value={fName} />
+      <input
+        type="text"
+        value={details.fName}
+        onChange={(e) => setDetails({ ...details, fName: e.target.value })}
+      />
 
       <label>Last Name</label>
-      <input type="text" value={lName} />
+      <input
+        type="text"
+        value={details.lName}
+        onChange={(e) => setDetails({ ...details, lName: e.target.value })}
+      />
 
       <label>Email</label>
-      <input type="email" value={email} />
+      <input
+        type="email"
+        value={details.email}
+        onChange={(e) => setDetails({ ...details, email: e.target.value })}
+      />
 
       <label>Phone</label>
-      <input type="tel" value={tel} />
-
-      <label>Country</label>
-      <input type="text" value={country} />
+      <input
+        type="tel"
+        value={details.tel}
+        onChange={(e) => setDetails({ ...details, tel: e.target.value })}
+      />
 
       <label>City</label>
-      <input type="text" value={city} />
+      <input
+        type="text"
+        value={details.city}
+        onChange={(e) => setDetails({ ...details, city: e.target.value })}
+      />
+
+      <label>Country</label>
+      <input
+        type="text"
+        value={details.country}
+        onChange={(e) => setDetails({ ...details, country: e.target.value })}
+      />
     </>
   );
 }
 
-function ProfessionalSummary({ summary }) {
+function ProfessionalSummary() {
+  const [summary, setSummary] = useState("");
   return (
     <>
       <h3>Professional Summary</h3>
@@ -58,42 +90,80 @@ function ProfessionalSummary({ summary }) {
         & most importantly - your biggest achievements, best qualities and
         skills.
       </p>
-      <textarea value={summary} name="" id="" cols="30" rows="4" />
+      <textarea
+        name=""
+        id=""
+        cols="30"
+        rows="4"
+        value={summary}
+        onChange={(e) => setSummary(e.target.value)}
+      />
     </>
   );
 }
 
-function EmploymentHistory({
-  jobTitle,
-  employer,
-  dateRange,
-  city,
-  description,
-}) {
+function EmploymentHistory() {
+  const [history, setHistory] = useState({
+    jobTitle: "",
+    employer: "",
+    dateRange: "",
+    city: "",
+    description: "",
+  });
   return (
     <>
       <h3>Employment History</h3>
       <p>Show your relevant experience.</p>
 
       <label>Job Title</label>
-      <input type="text" value={jobTitle} />
+      <input
+        type="text"
+        value={history.jobTitle}
+        onChange={(e) => setHistory({ ...history, jobTitle: e.target.value })}
+      />
 
       <label>Employer</label>
-      <input type="text" value={employer} />
+      <input
+        type="text"
+        value={history.employer}
+        onChange={(e) => setHistory({ ...history, employer: e.target.value })}
+      />
 
       <label>Start & End Date</label>
-      <input type="text" placeholder="MM/YY - MM/YY" value={dateRange} />
+      <input
+        type="text"
+        placeholder="MM/YY - MM/YY"
+        value={history.dateRange}
+        onChange={(e) => setHistory({ ...history, dateRange: e.target.value })}
+      />
 
       <label>City</label>
-      <input type="text" value={city} />
+      <input
+        type="text"
+        value={history.city}
+        onChange={(e) => setHistory({ ...history, city: e.target.value })}
+      />
 
       <label>Description</label>
-      <input type="text" value={description} />
+      <input
+        type="text"
+        value={history.description}
+        onChange={(e) =>
+          setHistory({ ...history, description: e.target.value })
+        }
+      />
     </>
   );
 }
 
-function Education({ school, degree, dateRange, city, description }) {
+function Education() {
+  const [education, setEducation] = useState({
+    school: "",
+    degree: "",
+    dateRange: "",
+    city: "",
+    description: "",
+  });
   return (
     <>
       <h3>Education</h3>
@@ -103,57 +173,102 @@ function Education({ school, degree, dateRange, city, description }) {
       </p>
 
       <label>School</label>
-      <input type="text" value={school} />
+      <input
+        type="text"
+        value={education.school}
+        onChange={(e) => setEducation({ ...education, school: e.target.value })}
+      />
 
       <label>Degree</label>
-      <input type="text" value={degree} />
+      <input
+        type="text"
+        value={education.degree}
+        onChange={(e) => setEducation({ ...education, degree: e.target.value })}
+      />
 
       <label>Start & End Date</label>
-      <input type="text" placeholder="MM/YY - MM/YY" value={dateRange} />
+      <input
+        type="text"
+        placeholder="MM/YY - MM/YY"
+        value={education.dateRange}
+        onChange={(e) =>
+          setEducation({ ...education, dateRange: e.target.value })
+        }
+      />
 
       <label>City</label>
-      <input type="text" value={city} />
+      <input
+        type="text"
+        value={education.city}
+        onChange={(e) => setEducation({ ...education, city: e.target.value })}
+      />
 
       <label>Description</label>
-      <input type="text" value={description} />
+      <input
+        type="text"
+        value={education.description}
+        onChange={(e) =>
+          setEducation({ ...education, description: e.target.value })
+        }
+      />
     </>
   );
 }
 
-function Skills({ skill, level }) {
-  return (
-    <>
-      <h3>Skills</h3>
-      <p>List your most desirable skills.</p>
-
-      <label>Skill</label>
-      <input type="text" value={skill} />
-
-      <label>Level</label>
-      <input type="range" value={level} />
-    </>
-  );
-}
-
-function References({ hide, referent, company, phone, email }) {
+function References() {
+  const [references, setReferences] = useState({
+    hide: false,
+    referent: "",
+    company: "",
+    phone: "",
+    email: "",
+  });
   return (
     <>
       <h3>References</h3>
 
       <label>Available upon request only.</label>
-      <input type="radio" value={hide} />
+      <input
+        type="radio"
+        value={references.hide}
+        onChange={(e) => setReferences({ ...references, hide: e.target.value })}
+      />
 
       <label>Referent&#39;s Full Name</label>
-      <input type="text" value={referent} />
+      <input
+        type="text"
+        value={references.referent}
+        onChange={(e) =>
+          setReferences({ ...references, referent: e.target.value })
+        }
+      />
 
       <label>Company</label>
-      <input type="text" value={company} />
+      <input
+        type="text"
+        value={references.company}
+        onChange={(e) =>
+          setReferences({ ...references, company: e.target.value })
+        }
+      />
 
       <label>Phone</label>
-      <input type="tel" value={phone} />
+      <input
+        type="tel"
+        value={references.phone}
+        onChange={(e) =>
+          setReferences({ ...references, phone: e.target.value })
+        }
+      />
 
       <label>Email</label>
-      <input type="email" value={email} />
+      <input
+        type="email"
+        value={references.email}
+        onChange={(e) =>
+          setReferences({ ...references, email: e.target.value })
+        }
+      />
     </>
   );
 }
