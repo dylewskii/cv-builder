6e82da838006ffb610a06eaaaf0c7b2cd229e76c
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-01-21 13:29:05 +0000

    add handleEmployee delete function

--- cv-builder/src/components/EmployeeEntry.jsx
+++ cv-builder/src/components/EmployeeEntry.jsx
@@ -1,15 +1,28 @@
 import entryCSS from "../styles/EmployeeEntry.module.css";
-import { FaTrash, FaEdit } from "react-icons/fa";
+import { FaTrash, FaEdit, FaAngleDown } from "react-icons/fa";
 
-export default function EmployeeEntry({ employment, setEmployees }) {
+export default function EmployeeEntry({ employment, setEmployment }) {
+  if (employment.length === 0) return;
+
+  function handleEmployeeDelete(empId) {
+    const index = employment.findIndex((emp) => emp.id === empId);
+    if (index !== -1) {
+      const updatedEmployment = [...employment];
+      updatedEmployment.splice(index, 1);
+      setEmployment(updatedEmployment);
+    }
+  }
+
   const allEntries = employment.map((emp, i) => (
     <div key={emp.id} className={entryCSS.employerEntry}>
       <h4>{emp.jobTitle ? emp.jobTitle : "Job Title goes here"}</h4>
       <div>
-        <FaEdit />
         <FaTrash
-          onClick={(e) => console.log("deleting object from employment state")}
+          className={entryCSS.icon}
+          onClick={() => handleEmployeeDelete(emp.id)}
         />
+        <FaEdit className={entryCSS.icon} />
+        <FaAngleDown className={entryCSS.icon} />
       </div>
     </div>
   ));
