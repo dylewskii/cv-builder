fc24b89378cb7d57bd52e0505dd7bc45c79f59a3
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-01-22 16:56:40 +0000

    EmploymentEntry is nowstyled and expands onClick

--- cv-builder/src/components/EmploymentEntry.jsx
+++ cv-builder/src/components/EmploymentEntry.jsx
@@ -21,30 +21,118 @@ export default function EmploymentEntry({
     setExpanded(!expanded);
   }
 
-  return (
-    <div className={eeCSS.employmentEntry}>
-      <h4>
-        {currentEmployment.jobTitle && currentEmployment.jobTitle}
-        {currentEmployment.employer && " - " + currentEmployment.employer}
-      </h4>
-      <div>
-        <FaTrash
-          className={eeCSS.icon}
-          onClick={() => handleEmploymentDelete()}
+  if (expanded) {
+    return (
+      <div
+        className={
+          expanded
+            ? `${eeCSS.employmentEntry} ${eeCSS.expanded}`
+            : eeCSS.employmentEntry
+        }
+      >
+        <h4>
+          {currentEmployment.jobTitle && currentEmployment.jobTitle}
+          {currentEmployment.employer && " - " + currentEmployment.employer}
+        </h4>
+        <div className={eeCSS.formExpanded}>
+          <div>
+            <label>Job Title</label>
+            <input
+              disabled
+              type="text"
+              name="jobTitle"
+              value={currentEmployment.jobTitle}
+            />
+          </div>
+
+          <div>
+            <label>Employer</label>
+            <input
+              disabled
+              type="text"
+              name="employer"
+              value={currentEmployment.employer}
+            />
+          </div>
+
+          <div>
+            <label>Start & End Date</label>
+            <input
+              disabled
+              type="text"
+              name="dateRange"
+              placeholder="MM/YY - MM/YY"
+              value={currentEmployment.dateRange}
+            />
+          </div>
+
+          <div>
+            <label>City</label>
+            <input
+              disabled
+              type="text"
+              name="city"
+              value={currentEmployment.city}
+            />
+          </div>
+
+          <div>
+            <label>Description</label>
+            <input
+              disabled
+              type="text"
+              name="description"
+              value={currentEmployment.description}
+            />
+          </div>
+        </div>
+        <EmploymentEntryControls
+          expanded={expanded}
+          handleEmploymentDelete={handleEmploymentDelete}
+          handleEmploymentExpand={handleEmploymentExpand}
         />
-        <FaEdit className={eeCSS.icon} onClick={() => console.log("editing")} />
-        {expanded ? (
-          <FaAngleUp
-            className={eeCSS.icon}
-            onClick={() => handleEmploymentExpand()}
-          />
-        ) : (
-          <FaAngleDown
-            className={eeCSS.icon}
-            onClick={() => handleEmploymentExpand()}
-          />
-        )}
       </div>
+    );
+  } else if (!expanded) {
+    return (
+      <div className={eeCSS.employmentEntry}>
+        <h4>
+          {currentEmployment.jobTitle && currentEmployment.jobTitle}
+          {currentEmployment.employer && " - " + currentEmployment.employer}
+        </h4>
+        <EmploymentEntryControls
+          expanded={expanded}
+          handleEmploymentDelete={handleEmploymentDelete}
+          handleEmploymentExpand={handleEmploymentExpand}
+        />
+      </div>
+    );
+  }
+}
+
+function EmploymentEntryControls({
+  expanded,
+  handleEmploymentDelete,
+  handleEmploymentExpand,
+}) {
+  return (
+    <div className={eeCSS.employmentEntryControls}>
+      <FaTrash
+        className={eeCSS.icon}
+        onClick={() => handleEmploymentDelete()}
+      />
+      <FaEdit className={eeCSS.icon} onClick={() => console.log("editing")} />
+      {expanded ? (
+        <FaAngleUp
+          className={eeCSS.icon}
+          onClick={() => handleEmploymentExpand()}
+        />
+      ) : (
+        <FaAngleDown
+          className={eeCSS.icon}
+          onClick={() => handleEmploymentExpand()}
+        />
+      )}
     </div>
   );
 }
