f594b5b1c82659b91c7f44e63c56d1e1fd20f7c3
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-01-30 18:31:34 +0000

    add CV (compound component)

---
+++ cv-builder/src/components/CV.jsx
@@ -0,0 +1,44 @@
+import cvList from "../styles/CVList.module.css";
+
+export default function CV({ children }) {
+  return <section className={cvList.cvSection}>{children}</section>;
+}
+
+// NEED TO ADD SRC AS AN ARGUMENT ONCE PDF PREVIEW HAS BEEN IMPLEMENTED
+CV.Preview = ({ onClick, children }) => {
+  return (
+    <div className={cvList.cvPreview} onClick={onClick}>
+      {/* <img alt="CV preview" /> */}
+      {children}
+    </div>
+  );
+};
+
+CV.Options = ({ children }) => {
+  return <div className={cvList.cvOptions}>{children}</div>;
+};
+
+CV.Name = ({ children }) => {
+  return <p className={cvList.cvName}>{children}</p>;
+};
+
+CV.Nav = ({}) => {
+  return (
+    <nav className={cvList.cvNav}>
+      <ul>
+        <li>
+          <a>Preview</a>
+        </li>
+        <li>
+          <a>Edit</a>
+        </li>
+        <li>
+          <a>Download</a>
+        </li>
+        <li>
+          <a>Delete</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
