d6d0254d00c89aadc5f41692d7055463d4e5d151
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-02-02 14:44:28 +0000

    add close icon to Modal

--- cv-builder/src/components/Modal.jsx
+++ cv-builder/src/components/Modal.jsx
@@ -1,17 +1,19 @@
 import modalCSS from "../styles/Modal.module.css";
+import { IoClose } from "react-icons/io5";
 
-export default function Modal({ visible, children }) {
+export default function Modal({ visible, onModalHide, children }) {
   if (!visible) return;
 
   return (
     <dialog className={modalCSS.modal} open={visible}>
+      <IoClose className={modalCSS.closeIcon} onClick={onModalHide} />
       {children}
     </dialog>
   );
 }
 
 Modal.Content = ({ children }) => {
-  return <div>{children}</div>;
+  return <div className={modalCSS.content}>{children}</div>;
 };
 
 Modal.Controls = ({ children }) => {
