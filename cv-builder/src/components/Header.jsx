dec3012ea092ccb5a005ec0ab79081f951d97dab
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-02-03 14:16:19 +0000

    add & style logo

--- cv-builder/src/components/Header.jsx
+++ cv-builder/src/components/Header.jsx
@@ -1,11 +1,14 @@
 import HeaderCSS from "../styles/Header.module.css";
 import { CgProfile } from "react-icons/cg";
+import { GiVineLeaf } from "react-icons/gi";
 
 export default function Header() {
   return (
     <>
       <header className={HeaderCSS.header}>
-        <h1>VitaeVine</h1>
+        <h1>
+          <GiVineLeaf /> VitaeVine
+        </h1>
         <CgProfile className={HeaderCSS.profileIcon} />
       </header>
     </>
