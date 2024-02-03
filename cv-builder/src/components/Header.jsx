1ce1c1df3ba43f6d4620135627032f47c80ddf4e
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-02-03 14:09:54 +0000

    update Header title

--- cv-builder/src/components/Header.jsx
+++ cv-builder/src/components/Header.jsx
@@ -1,17 +1,12 @@
 import HeaderCSS from "../styles/Header.module.css";
 import { CgProfile } from "react-icons/cg";
-import { IconContext } from "react-icons";
 
 export default function Header() {
   return (
     <>
       <header className={HeaderCSS.header}>
-        <h1>CanVas</h1>
-        <IconContext.Provider
-          value={{ size: "2rem", className: HeaderCSS.profileIcon }}
-        >
-          <CgProfile />
-        </IconContext.Provider>
+        <h1>VitaeVine</h1>
+        <CgProfile className={HeaderCSS.profileIcon} />
       </header>
     </>
   );
